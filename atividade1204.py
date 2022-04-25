c01609d1bbc94475ffa5e30cf514234c43d4d8c4
Tarcisio <tarcisioalmeida1358@gmail.com>
2022-04-25 19:36:28 -0300

    pequena atualização do código

--- atividade1204.py
+++ atividade1204.py
@@ -1,15 +1,15 @@
 from prettytable import PrettyTable
 
-x = PrettyTable()
-y = PrettyTable()
-x.field_names = ["Produto","Quantidade mínima","Quantidade máxima", "Quantidade atual", "Valor unitário"]
-y.field_names = ["Produto","Quantidade mínima","Quantidade máxima", "Quantidade atual", "Valor unitário"]
+mercTotal = PrettyTable()
+mercReposicao = PrettyTable()
+mercTotal.field_names = ["Produto","Quantidade mínima","Quantidade máxima", "Quantidade atual", "Valor unitário"]
+mercReposicao.field_names = ["Produto","Quantidade mínima","Quantidade máxima", "Quantidade atual", "Valor unitário"]
 
 def mercadoriaTotal(nome, quantMin, quantMax, quantAtual, valorUni):
 
-    x.add_row([nome, quantMin, quantMax, quantAtual, valorUni])
-    return x
+    mercTotal.add_row([nome, quantMin, quantMax, quantAtual, valorUni])
+    return mercTotal
 
 def mercadoriaReposicao(nome, quantMin, quantMax, quantAtual, valorUni):
-    y.add_row([nome, quantMin, quantMax, quantAtual, valorUni])
-    return y
+    mercReposicao.add_row([nome, quantMin, quantMax, quantAtual, valorUni])
+    return mercReposicao
